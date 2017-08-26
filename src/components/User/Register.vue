4af1eb2cede990db7680e080cc103f41b7923f73
Eridanus Sora <haolinaihui@Gmail.com>
2017-08-26 21:44:52 +0800

    finish user register

--- src/components/User/Register.vue
+++ src/components/User/Register.vue
@@ -1,6 +1,57 @@
 <template>
-  <div>11</div>
+  <el-card>
+    <el-form @keyup.enter.native="submit" v-loading="loading">
+      <h2>注册</h2>
+      <el-form-item label="队伍名">
+        <el-input placeholder="队伍名" v-model="form.teamName"></el-input>
+      </el-form-item>
+      <el-form-item label="Email">
+        <el-input placeholder="Email" v-model="form.email"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" placeholder="密码" v-model="form.password1"></el-input>
+      </el-form-item>
+      <el-form-item label="重复密码">
+        <el-input type="password" placeholder="重复 密码" v-model="form.password2"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="submit">提交</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 <script>
-
+  import User from '../../model/User';
+  let UserModel = new User();
+  export default {
+    data() {
+      return {
+        form: {
+          teamName: "",
+          email: "",
+          password1: "",
+          password2: ""
+        },
+        loading: false
+      }
+    },
+    methods: {
+      async submit() {
+        if (!this.form.teamName || !this.form.email || !this.form.password1 || !this.form.password2) {
+          return this.$message.error("请填写表单所有项目");
+        }
+        if (this.form.password1 !== this.form.password2) {
+          return this.$message.error("两次填写的密码不一致");
+        }
+        this.loading = true;
+        try{
+          let result = await UserModel.register(this.form.teamName, this.form.email, this.form.password2);
+        }
+        catch(e){
+          this.$message.error(e.message);
+        }
+        this.loading = false;
+      }
+    }
+  }
 </script>
--- src/model/User.js
+++ src/model/User.js
@@ -2,19 +2,44 @@ import makeHash from 'sha.js';
 
 import Model from './model'
 class User extends Model{
+  /**
+   * Do SHA-256 Hashing
+   * @param text
+   * @returns {*}
+   */
   sha256(text = ''){
     let sha256 = makeHash('sha256');
     return sha256.update(text).digest('hex');
   }
-  login(email, password){
-    let self = this;
+
+  /**
+   * 登陆
+   * @param email
+   * @param password
+   * @returns {Promise}
+   */
+  login(teamName, email, password){
     return new Promise(async (resolve, reject) => {
       try{
         let result = await this.request("POST", '/User/login', {
+          teamName: teamName,
           email: email,
           password: this.sha256(password)
         });
         resolve(result);
+      }
+      catch (e){
+        reject(e);
+      }
+    })
+  }
+  register(email, password){
+    return new Promise(async (resolve, reject) => {
+      try{
+        let result = await  this.request("POST", "/User/register", {
+          email: email,
+          password: password
+        })
       }
       catch (e){
         reject(e);
