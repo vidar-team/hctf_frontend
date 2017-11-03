59ae085c23b94d6a5b664e027d15cacb35186260
Eridanus Sora <haolinaihui@Gmail.com>
2017-11-03 18:15:42 +0800

    add rules

--- src/components/Common/Bulletin.vue
+++ src/components/Common/Bulletin.vue
@@ -1,5 +1,49 @@
 <template>
-  <h2>{{ $t("bulletin.title") }} </h2>
+  <div>
+    <h2>{{ $t("bulletin.title") }} </h2>
+    <h3>Rules</h3>
+    <article>
+      <ul>
+        <li>Qualification is online jeopardy for 48 hours.</li>
+        <li>Top 10 Chinese teams will be invited to HCTF Final.</li>
+        <li>After the HCTF Final, we will invite two team from Zhejiang to take part in an extra-competition.</li>
+        <li>Dynamic Score System is applied to all the challenges. All the challenges have an initial score of 1,000 and will be adjusted automatically according to the number of teams who solved the challenge.</li>
+        <li>Each team has 30 chances to submit flag for each challenge.</li>
+        <li>
+          Flag format: hctf{xxxxxxx}
+        </li>
+        <li>
+          During the contest,any of the following behaviors will lead to disqualification:
+          <ol>
+            <li>Any behavior that compromises the fairness of contest</li>
+            <li>Publishing, exchanging problem solutions</li>
+            <li>Attacking any personnel, including participants and non-participants, or the contest system itself</li>
+          </ol>
+        </li>
+        <li>A special rule will be introduced 2 hours before the qualification.</li>
+        <li>The qualification starts at 2017-11-11T12:00:00.000Z and ends at 2017-11-13T12:00:00.000Z. (48hrs)</li>
+      </ul>
+    </article>
+    <article>
+      <ul>
+        <li>预赛为48小时的线上 jeopardy 形式</li>
+        <li>前十的中国队伍将邀请参加 HCTF 决赛</li>
+        <li>HCTF 决赛结束后，还会邀请两支浙江的队伍和决赛队伍一起参加加赛</li>
+        <li>题目分数采用动态分数，每题初始分值为 1,000 ，根据题目完成情况，题目的分值会发生变化。</li>
+        <li>对于每道题目，每支队伍仅有30次提交 flag的机会</li>
+        <li>Flag格式: hctf{xxxxxxx}</li>
+        <li>比赛期间，做出以下事项的参赛队伍将会被取消资格:
+          <ol>
+            <li>进行任何破坏比赛公平公正的行为</li>
+            <li>散播或与其他队伍交换flag</li>
+            <li>对平台、参赛者及其他人员进行攻击</li>
+          </ol>
+        </li>
+        <li>比赛采用特殊的规则，神秘的规则将在比赛开始前2个小时公布</li>
+        <li>预赛时间为 2017-11-10T12:00:00.000Z 到 2017-11-12T12:00:00.000Z （共48小时）</li>
+      </ul>
+    </article>
+  </div>
 </template>
 <script>
   export default {}
