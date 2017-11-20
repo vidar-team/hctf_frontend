7291699e5585b1cd9096918ea43bbe7c560bcded
Eridanus Sora <haolinaihui@Gmail.com>
2017-11-20 22:13:22 +0800

    challenge list componetilize

--- src/components/Admin/Challenge/Level.vue
+++ src/components/Admin/Challenge/Level.vue
@@ -20,53 +20,7 @@
         </el-form>
       </el-tab-pane>
       <el-tab-pane label="Challenge 管理">
-        <el-table :data="level.challenges">
-          <el-table-column type="expand">
-            <template slot-scope="scope">
-              <el-form label-position="left" inline class="table-expand">
-                <el-form-item label="Level ID">
-                  <span>{{ scope.row.level_id }}</span>
-                </el-form-item>
-                <el-form-item label="描述">
-                  <span>{{ scope.row.description }}</span>
-                </el-form-item>
-                <el-form-item label="基准分数">
-                  <span>{{ scope.row.score }}</span>
-                </el-form-item>
-                <el-form-item label="URL">
-                  <span>{{ scope.row.url }}</span>
-                </el-form-item>
-                <el-form-item label="发布时间">
-                  <span>{{ scope.row.release_time }}</span>
-                </el-form-item>
-                <el-form-item label="动态 Flag">
-                  <span>{{ scope.row.is_dynamic_flag }}</span>
-                </el-form-item>
-                <el-form-item label="最小解决时间">
-                  <span>{{ JSON.parse(scope.row.config).minimumSolveTime === 0 ? '不限制' : JSON.parse(scope.row.config).minimumSolveTime + '秒'}}</span>
-                </el-form-item>
-              </el-form>
-            </template>
-          </el-table-column>
-          <el-table-column
-            prop="challenge_id"
-            label="Challenge ID"
-          >
-          </el-table-column>
-          <el-table-column
-            prop="title"
-            label="标题"
-          >
-          </el-table-column>
-          <el-table-column
-            label="操作"
-          >
-            <template slot-scope="scope">
-              <el-button type="text" @click="editChallenge(scope.row.challenge_id)">编辑</el-button>
-              <el-button type="text" style="color: red" @click="deleteChallenge(scope.row.challenge_id)">删除</el-button>
-            </template>
-          </el-table-column>
-        </el-table>
+        <challenge-list :challenges="level.challenges"></challenge-list>
       </el-tab-pane>
       <el-tab-pane label="开放条件设置" name="rules" v-loading="loading">
         <rules mode="edit" :rules="this.level.rules" :info="info" @update="updateRule"></rules>
@@ -93,12 +47,11 @@
 <script>
   import Level from '@/model/admin/Level';
   import Category from '@/model/admin/Category';
-  import Challenge from '@/model/admin/Challenge';
   import Rules from './RuleComponents/Rules.vue';
+  import ChallengeList from './List.vue';
 
   let LevelModel = new Level();
   let CategoryModel = new Category();
-  let ChallengeModel = new Challenge();
   export default {
     data() {
       return {
@@ -125,7 +78,8 @@
       this.loadLevelInfo(this.$route.query.id);
     },
     components: {
-      Rules
+      Rules,
+      ChallengeList
     },
     methods: {
       async loadLevelInfo(levelId) {
@@ -169,30 +123,6 @@
         }
         this.loading = false;
       },
-      async deleteChallenge(challengeId) {
-        this.loading = true;
-        try {
-          await this.$confirm('本操作将会删除该问题及其所有关联Flag、关联答题记录，全部队伍的分数将会重新计算，生产环境下不建议进行此操作，是否确认？', '危险操作确认', {
-            confirmButtonText: '确定',
-            cancelButtonText: '取消',
-            type: 'warning'
-          });
-          await ChallengeModel.deleteChallenge(challengeId);
-          this.loadLevelInfo(this.$route.query.id);
-        }
-        catch (e) {
-          this.$handleError(e);
-        }
-        this.loading = false;
-      },
-      editChallenge(challengeId) {
-        this.$router.push({
-          name: "Admin-Challenge-Edit",
-          query: {
-            challengeId: challengeId
-          }
-        })
-      }
     }
   }
 </script>
---
+++ src/components/Admin/Challenge/List.vue
@@ -0,0 +1,87 @@
+<template>
+  <el-table :data="challenges">
+    <el-table-column type="expand">
+      <template slot-scope="scope">
+        <el-form label-position="left" inline class="table-expand">
+          <el-form-item label="Level ID">
+            <span>{{ scope.row.level_id }}</span>
+          </el-form-item>
+          <el-form-item label="描述">
+            <span>{{ scope.row.description }}</span>
+          </el-form-item>
+          <el-form-item label="基准分数">
+            <span>{{ scope.row.score }}</span>
+          </el-form-item>
+          <el-form-item label="URL">
+            <span>{{ scope.row.url }}</span>
+          </el-form-item>
+          <el-form-item label="发布时间">
+            <span>{{ scope.row.release_time }}</span>
+          </el-form-item>
+          <el-form-item label="动态 Flag">
+            <span>{{ scope.row.is_dynamic_flag }}</span>
+          </el-form-item>
+          <el-form-item label="最小解决时间">
+            <span>{{ JSON.parse(scope.row.config).minimumSolveTime === 0 ? '不限制' : JSON.parse(scope.row.config).minimumSolveTime + '秒'}}</span>
+          </el-form-item>
+        </el-form>
+      </template>
+    </el-table-column>
+    <el-table-column
+      prop="challenge_id"
+      label="Challenge ID"
+    >
+    </el-table-column>
+    <el-table-column
+      prop="title"
+      label="标题"
+    >
+    </el-table-column>
+    <el-table-column
+      label="操作"
+    >
+      <template slot-scope="scope">
+        <el-button type="text" @click="editChallenge(scope.row.challenge_id)">编辑</el-button>
+        <el-button type="text" style="color: red" @click="deleteChallenge(scope.row.challenge_id)">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+<script>
+  import Challenge from '@/model/admin/Challenge';
+  let ChallengeModel = new Challenge();
+  export default {
+    data(){
+      return {
+
+      }
+    },
+    props: ["challenges"],
+    methods: {
+      editChallenge(challengeId) {
+        this.$router.push({
+          name: "Admin-Challenge-Edit",
+          query: {
+            challengeId: challengeId
+          }
+        })
+      },
+      async deleteChallenge(challengeId) {
+        this.loading = true;
+        try {
+          await this.$confirm('本操作将会删除该问题及其所有关联Flag、关联答题记录，全部队伍的分数将会重新计算，生产环境下不建议进行此操作，是否确认？', '危险操作确认', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning'
+          });
+          await ChallengeModel.deleteChallenge(challengeId);
+          this.loadLevelInfo(this.$route.query.id);
+        }
+        catch (e) {
+          this.$handleError(e);
+        }
+        this.loading = false;
+      },
+    }
+  }
+</script>
